bdfa4239fb42cbd4bc74ba9bcadccb07d85e1fa9
Toon Verbeek <toon.verbeek1@gmail.com>
2014-03-22 16:21:09 +0100

    changes to layout and scaling propeprly

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
 app.controller('PetitionCtrl', function ($scope, Petition) {
-        $scope.petition = {name: '', surname: '', email: ''};
+        $scope.petition = {name: '', surname: '', email: '', text: ''};
         $scope.petitions = Petition.all;
 
         $scope.submit = function () {
             Petition.create($scope.petition).then(function (ref){
                 //todo: reroute to correct page.
-                $scope.petition = {name: '', surname: '', email: ''};
+                $scope.petition = {name: '', surname: '', email: '', text:''};
             });
         };
 
