9eeb59b5da420727bf3dc45ebef8c5b7996b09b5
Toon Verbeek <toon.verbeek1@gmail.com>
2014-03-30 15:24:27 +0200

    design tweaks and font

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,25 @@
 'use strict';
 
-app.controller('PetitionCtrl', function ($scope, Petition) {
-        $scope.petition = {name: '', email: '', text: ''};
-        $scope.petitions = Petition.all;
+app.controller('PetitionCtrl', function($scope, Petition) {
+    $scope.petition = {name: '', email: '', text: ''};
+    $scope.petitions = Petition.all;
+    $scope.amountOfPetitions = $scope.petitions.$getIndex().length;
+    $scope.submit = function() {
+        Petition.create($scope.petition).then(function(ref) {
+            //todo: reroute to correct page.
 
-        $scope.submit = function () {
-            Petition.create($scope.petition).then(function (ref){
-                //todo: reroute to correct page.
-                $scope.petition = {name: '', email: '', text:''};
-            });
-        };
+            console.log($scope.petitions.count() + ' have already signed up.');
+            $scope.successsful = true;
+            $scope.petition = {name: '', email: '', text: ''};
+            $scope.amountOfPetitions = $scope.petitions.$getIndex().length;
+            console.log(Petition.all.$getIndex().length);
+        });
 
-    });
+    };
+
+    $scope.init = function() {
+        $scope.amountOfPetitions = $scope.petitions.$getIndex().length;
+
+    };
+
+});
--- app/scripts/services/petition.js
+++ app/scripts/services/petition.js
@@ -3,26 +3,62 @@
  */
 
 'use strict';
-app.factory('Petition', function ($firebase, FIREBASE_URL) {
-        var ref = new Firebase(FIREBASE_URL + '/jongduurzaam'); //create a reference to the correct database
+app.factory('Petition', function($firebase, FIREBASE_URL) {
+    var ref = new Firebase(FIREBASE_URL + '/jongduurzaam'); //create a reference to the correct database
 
-        var petitions = $firebase(ref); //create the reference to the json object containing the petitions
+    var petitions = $firebase(ref); //create the reference to the json object containing the petitions
 
-        var Petition = {
-            all: petitions,
-            create: function (petition) {
-                return petitions.$add(petition);
-            },
-            delete: function(petitionId){
-                return petitions.$remove(petitionId);
-            },
-            find: function(petitionId){
-                return petitions.$child(petitionId);
-            },
-            update: function(petitionId){
-
+    var Petition = {
+        all: petitions,
+        create: function(petition) {
+            var keys = petitions.$getIndex();
+            var exists = false;
+            for (var i = 0; i < keys.length; i++) {
+                console.log(petitions[keys[i]['email']]);
+                if (petitions[keys[i]]['email'] === petition.email) {
+                    console.log('found email key' + petition.email);
+                    exists = true;
+                    break;
+                }
             }
-        };
+            if (!exists) {
+                $('#alert-placeholder-failure').hide();
+                $('#alert-placeholder-success').show();
+                return petitions.$add(petition);
+            }
+            else {
+                $('#alert-placeholder-success').hide();
+                $('#alert-placeholder-failure').show();
+                return new Error("Dit e-mail adres staat al geregistreerd in ons systeem!");
+            }
+        },
+        delete: function(petitionId) {
+            return petitions.$remove(petitionId);
+        },
+        find: function(petitionId) {
+            return petitions.$child(petitionId);
+        },
+        update: function(petitionId) {
 
-        return Petition;
-    });
+        },
+        count: function() {
+            while (petitions.$getIndex() === 0)
+                ;
+            return petitions.$getIndex().length;
+        }
+    };
+
+    return Petition;
+});
+
+
+
+function escapeEmailAddress(email) {
+    if (!email)
+        return false;
+
+    // Replace '.' (not allowed in a Firebase key) with ','
+    email = email.toLowerCase();
+    email = email.replace(/\./g, ',');
+    return email;
+}
