d29abebc025d95e74a2df461fa226a67c2a4bf70
Toon Verbeek <toon.verbeek1@gmail.com>
2014-04-05 16:45:57 +0200

    added some new content anda navbar

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,25 +1,46 @@
 'use strict';
 
-app.controller('PetitionCtrl', function($scope, Petition) {
+app.controller('PetitionCtrl', function($scope, $location, $anchorScroll, Petition) {
     $scope.petition = {name: '', email: '', text: ''};
     $scope.petitions = Petition.all;
     $scope.amountOfPetitions = $scope.petitions.$getIndex().length;
     $scope.submit = function() {
-        Petition.create($scope.petition).then(function(ref) {
-            //todo: reroute to correct page.
+        if ($scope.petition.name !== '' && $scope.petition.email !== '' && $scope.petition.email.indexOf('@') !== -1 && $scope.petition.email.indexOf('.') !== -1
+                && $scope.petition.email.length >= 5) {
+            if ($scope.petition.text.length === 0) {
+                $scope.petition.text = 'Ik steun deze petitie!';
+            }
+            Petition.create($scope.petition).then(function(ref) {
+                //todo: reroute to correct page.
 
-            console.log($scope.petitions.count() + ' have already signed up.');
-            $scope.successsful = true;
-            $scope.petition = {name: '', email: '', text: ''};
-            $scope.amountOfPetitions = $scope.petitions.$getIndex().length;
-            console.log(Petition.all.$getIndex().length);
-        });
+                console.log($scope.petitions.count() + ' have already signed up.');
+                $scope.successsful = true;
+                $scope.petition = {name: '', email: '', text: ''};
+                $scope.amountOfPetitions = $scope.petitions.$getIndex().length;
+                console.log(Petition.all.$getIndex().length);
+            });
+        } else if ($scope.petition.email.indexOf('@') === -1 || $scope.petition.email.indexOf('.') === -1 || $scope.petition.email.length < 5) {
+            $('#alert-placeholder-success').hide();
+            $('#alert-placeholder-failure').html('Voer een geldig e-mail adres in.');
+            $('#alert-placeholder-failure').show();
+        } else {
+            $('#alert-placeholder-success').hide();
+            $('#alert-placeholder-failure').html('Zowel je naam als e-mail adres zijn vereist.');
+            $('#alert-placeholder-failure').show();
+        }
 
     };
 
     $scope.init = function() {
         $scope.amountOfPetitions = $scope.petitions.$getIndex().length;
 
+    };
+
+    $scope.goToElement = function(navitem, category) {
+        //$('.active').toggleClass('active');
+        //document.getElementById(navitem).className = 'active';
+        $location.hash(category);
+        $anchorScroll();
     };
 
 });
